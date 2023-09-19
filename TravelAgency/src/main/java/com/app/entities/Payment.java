7fff28ece0d7a656efe02c0abf99122c81f43100
Abhishek-Shandilya-03 <131249768+Abhishek-Shandilya-03@users.noreply.github.com>
2023-09-19 22:31:35 +0530

    Add files via upload

---
+++ TravelAgency/src/main/java/com/app/entities/Payment.java
@@ -0,0 +1,50 @@
+package com.app.entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Payment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long paymentId;
+	
+	private double amount;
+	@Enumerated(EnumType.STRING)
+	@NotNull
+	private PaymentMethod paymentMethod;
+	@Column(length = 20)
+	@DateTimeFormat( pattern = "yyyy-mm-dd")
+	private LocalDate paymentdate;
+	
+//	 @OneToOne
+//	    @JoinColumn(name="bookingId")
+//	    private Booking booking;
+}
